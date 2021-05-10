0dc6728956919d23365e06cd24fc5fba63c84968
JamesZ <jiayang_zhao@ucsb.edu>
2021-05-10 09:31:43 +0800

    Add files via upload

---
+++ psCombo.cpp
@@ -0,0 +1,19 @@
+#include <iomanip>
+#include "psCombo.h"
+
+
+
+/* print state data - as aggregate of all incidents in this state */
+std::ostream& operator<<(std::ostream &out, const psCombo& PD) {
+    out << "Combo Info: " << PD.printState();
+    out << "\nNumber of incidents: " << PD.getNumberOfCases();
+    out << std::setprecision(2) << std::fixed;
+    out << "\nIncidents with age \n(over 65): " << PD.getCasesOver65();
+    out << "\n(19 to 64): " << PD.getNumberOfCases()-PD.getCasesOver65()-PD.getCasesUnder18();
+    out << "\n(under 18): " << PD.getCasesUnder18();
+    out << "\nIncidents involving fleeing: " << PD.getFleeingCount();
+    out << "\nIncidents involving mental illness: " << PD.getNumMentalI();
+    out << "\nMale incidents: " << PD.getnumMales() << " female incidents: " << PD.getnumFemales();
+    out << "\nRacial demographics of state incidents: " << PD.getRacialData();
+    return out;
+}
