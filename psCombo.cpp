ae0434f7f346cd71ba8e32c8cd7fa460a55d32bf
JamesZ <jiayang_zhao@ucsb.edu>
2021-05-13 21:00:30 +0800

    Update psCombo.cpp

--- psCombo.cpp
+++ psCombo.cpp
@@ -5,7 +5,7 @@
 
 /* print state data - as aggregate of all incidents in this state */
 std::ostream& operator<<(std::ostream &out, const psCombo& PD) {
-    out << "Combo Info: " << PD.printState();
+    out << "State Info: " << PD.printState();
     out << "\nNumber of incidents: " << PD.getNumberOfCases();
     out << std::setprecision(2) << std::fixed;
     out << "\nIncidents with age";
