2cc9f88dfbaf247bd6c6f8dbe6a7bf7965656ac9
JamesZ <jiayang_zhao@ucsb.edu>
2021-05-13 20:58:00 +0800

    Update psCombo.cpp

--- psCombo.cpp
+++ psCombo.cpp
@@ -8,11 +8,12 @@ std::ostream& operator<<(std::ostream &out, const psCombo& PD) {
     out << "Combo Info: " << PD.printState();
     out << "\nNumber of incidents: " << PD.getNumberOfCases();
     out << std::setprecision(2) << std::fixed;
-    out << "\nIncidents with age \n(over 65): " << PD.getCasesOver65();
-    out << "\n(19 to 64): " << PD.getNumberOfCases()-PD.getCasesOver65()-PD.getCasesUnder18();
-    out << "\n(under 18): " << PD.getCasesUnder18();
-    out << "\nIncidents involving fleeing: " << PD.getFleeingCount();
-    out << "\nIncidents involving mental illness: " << PD.getNumMentalI();
+    out << "\nIncidents with age";
+    out << "\n(over 65): " << PD.getCasesOver65() << " %: " << PD.getCasesOver65()/double(PD.getNumberOfCases())*100;
+    out << "\n(19 to 64): " << PD.getNumberOfCases()-PD.getCasesOver65()-PD.getCasesUnder18() << " %: " << (PD.getNumberOfCases()-PD.getCasesOver65()-PD.getCasesUnder18())/double(PD.getNumberOfCases())*100;
+    out << "\n(under 18): " << PD.getCasesUnder18() << " %: " << PD.getCasesUnder18()/double(PD.getNumberOfCases())*100;
+    out << "\nIncidents involving fleeing: " << PD.getFleeingCount() << " %: " << PD.getFleeingCount()/double(PD.getNumberOfCases())*100;
+    out << "\nIncidents involving mental illness: " << PD.getNumMentalI() << " %: " << PD.getNumMentalI()/double(PD.getNumberOfCases())*100;
     out << "\nMale incidents: " << PD.getnumMales() << " female incidents: " << PD.getnumFemales();
     out << "\nRacial demographics of state incidents: " << PD.getRacialData();
     return out;
