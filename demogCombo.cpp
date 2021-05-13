ce231754f47f507ba57563d91cd7808568050742
JamesZ <jiayang_zhao@ucsb.edu>
2021-05-13 20:41:11 +0800

    Update demogCombo.cpp

--- demogCombo.cpp
+++ demogCombo.cpp
@@ -14,16 +14,16 @@ std::ostream& operator<<(std::ostream &out, const demogCombo&SD) {
     out << "\nNumber of Counties: " << SD.getNumberofcounty() << " County Demographics Info: "
         << "comboData, " << SD.getState();
     out << std::setprecision(2) << std::fixed;
-    out << "\nPopulation info: \n(\% over 65): " << SD.getpopOver65()<< "\%";
+    out << "\nPopulation info: \n(over 65): " << SD.getpopOver65()<< "\%";
     out <<  " and total: " << SD.getpopOver65Count();
-    out << "\n(\% under 18): " << SD.getpopUnder18()<<"\%";
+    out << "\n(under 18): " << SD.getpopUnder18()<<"\%";
     out <<  " and total: " << SD.getpopUnder18Count();
-    out << "\n(\% under 5): " << SD.getpopUnder5()<<"\%";
+    out << "\n(under 5): " << SD.getpopUnder5()<<"\%";
     out <<  " and total: " << SD.getpopUnder5Count();
     out << "\nEducation info: ";
-    out << "\n(\% Bachelor degree or more): " << SD.getBAup()<<"\%";
+    out << "\n(Bachelor degree or more): " << SD.getBAup()<<"\%";
     out <<  " and total: " << SD.getBAupCount();
-    out << "\n(\% high school or more): " << SD.getHSup()<<"\%";
+    out << "\n(high school or more): " << SD.getHSup()<<"\%";
     out <<  " and total: " << SD.getHSupCount();
     out << "\npersons below poverty: " << SD.getBelowPoverty()<<"\%";
     out <<  " and total: " << SD.getpopPoorCount();
